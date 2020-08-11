75d617e0c913c45b46fd51d6881268a64cf8e092
André Yuri <andre_yuri@live.com>
2020-08-10 23:57:17 -0300

    Creating transations service

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,7 +1,8 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
+
+import CreateTransactionService from '../services/CreateTransactionService';
 
 // import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
@@ -11,8 +12,19 @@ transactionsRouter.get('/', async (request, response) => {
   // TODO
 });
 
-transactionsRouter.post('/', async (request, response) => {
-  // TODO
+transactionsRouter.post('/', async (request: Request, response: Response) => {
+  const { title, value, type, category } = request.body;
+
+  const createTransaction = new CreateTransactionService();
+
+  const transaction = await createTransaction.execute({
+    title,
+    value,
+    type,
+    categoryTitle: category,
+  });
+
+  return response.json(transaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
